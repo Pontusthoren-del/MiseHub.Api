cc01cd59a9486b691dc02450edf6a68ff6991433
Pontus <Pontus_thoren@hotmail.com>
2026-09-07 10:49:07 +0200

    LoginRequest Dto

--- DTOs/LoginRequest.cs
+++ DTOs/LoginRequest.cs
@@ -1,8 +1,13 @@
-﻿namespace MiseHub.Api.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiseHub.Api.DTOs;
 
 
 public class LoginRequest
 {
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = string.Empty;
+    [Required]
     public string Password { get; set; } = string.Empty;
 }
