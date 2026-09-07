12cf731bffa9c7a25de67dbea944bf41b1e602d0
Pontus <Pontus_thoren@hotmail.com>
2026-09-07 10:51:16 +0200

    RegisterRequest dto

--- DTOs/RegisterRequest.cs
+++ DTOs/RegisterRequest.cs
@@ -8,7 +8,9 @@ public class RegisterRequest
     [EmailAddress]
     public string Email { get; set; } = string.Empty;
     [Required]
+    [StringLength(100,MinimumLength =8,ErrorMessage ="Password must atleast be 8 characters.")]
     public string Password { get; set; } = string.Empty;
     [Required]
+    [StringLength(100,MinimumLength =1)]
     public string RestaurantName { get; set; } = string.Empty;
 }
