cde96bb8b5eac395ab4c61211287d0e7bf163060
Pontus <Pontus_thoren@hotmail.com>
2026-09-09 09:34:10 +0200

    Building user model

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace MiseHub.Api.Models;
+
+public class User
+{
+    public string Id { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string RestaurantName { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+}
